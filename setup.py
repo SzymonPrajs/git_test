fedff21b03d37a0097e709bee1408ffd3b54dd50
Szymon Prajs <S.Prajs@soton.ac.uk>
2017-04-24 15:28:11 -0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+
+#!/Users/szymon/anaconda/envs/python36/bin/python
+
+from distutils.core import setup
+
+setup(name='MyPiPy',
+      version='0.1dev',
+      description='First dev verson',
+      author='Szymon Prajs',
+      author_email='S.Prajs@soton.ac.uk',
+      packages=['MyPiPy'],
+     ) 
